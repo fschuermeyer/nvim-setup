0ebd57202dc7cf51ed34489bdda6b878e376a90f
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-04 15:02:34 +0200

    alternaitve move up/down (macos)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Move Lines
+map("n", "<C-m>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("n", "<C-n>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("i", "<C-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<C-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<C-n>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<C-m>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
