b0cc4076acaab4ee4da310ac322c72dc2f7a8ac0
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-04 14:40:06 +0200

    improvments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "node_modules" },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          go_test_args = { "-v", "-race", "-count=1" },
+          dep_go_enabled = true,
+        },
+      },
+    },
+  },
+}
