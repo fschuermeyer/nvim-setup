70fcd2f61c2f2efe447e5365db7c00a4d03bf267
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-09 22:28:57 +0200

    adding mason lock plugin to support - mason sync over repository

---
+++ lua/plugins/mason-lock.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "zapling/mason-lock.nvim",
+    init = function()
+      require("mason-lock").setup({
+        lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json", -- (default)
+      })
+    end,
+  },
+}
