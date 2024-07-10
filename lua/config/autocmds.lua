c938c38d4756d882ef94c8c24ff3cde059c2a07d
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-10 19:26:59 +0200

    configure gohtml syntax highlighting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+  autocmd BufNewFile,BufRead *.tmpl set filetype=gohtmltmpl
+  autocmd BufNewFile,BufRead *.gohtml set filetype=gohtmltmpl
+  autocmd BufNewFile,BufRead *.gohtmltmpl set filetype=gohtmltmpl
+  autocmd BufNewFile,BufRead *.gotexttmpl  set filetype=gotexttmpl
+]])
