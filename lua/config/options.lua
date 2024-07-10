33c10a2c8a57cfca6c8f06815068a5a1e2c10080
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-10 12:38:19 +0200

    move telescope configuration to additional file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "node_modules", "swaggerui-src/.*" },
-      },
-    },
-  },
   {
     "nvim-neotest/neotest",
     opts = {
