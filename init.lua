30ea99c671172864e39e8b575999f570593fb63a
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-10 12:00:24 +0200

    switch colorscheme

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- provider setup
 vim.g.python3_host_prog = os.getenv("HOME") .. "/.pyenv/versions/3.11.0/bin/python3"
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+-- define the colorscheme for nvim
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
