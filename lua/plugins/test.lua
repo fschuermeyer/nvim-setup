768c93d6562129673c292195a6fb202b2b999586
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-16 17:41:50 +0200

    update tests

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,3 +1,36 @@
 return {
-  "nvim-neotest/neotest-jest",
+  { "fredrikaverpil/neotest-golang", enabled = false },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-jest",
+    },
+    config = function()
+      local goAdapter = require("neotest-go")({
+        args = { "-v" },
+      })
+
+      local pyAdapter = require("neotest-python")({
+        args = { "-s", "-vv" },
+        runner = "pytest",
+      })
+
+      local jestAdapter = require("neotest-jest")({})
+
+      ---@diagnostic disable-next-line: missing-fields
+      require("neotest").setup({
+        adapters = { goAdapter, pyAdapter, jestAdapter },
+        ---@diagnostic disable-next-line: missing-fields
+        diagnostic = {
+          enable = true,
+        },
+      })
+    end,
+  },
 }
