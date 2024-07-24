b8673b26de52c50cd54d5c8bbbd7af254b93aa59
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-07-24 08:52:08 +0200

    remove lazygit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,29 +2,42 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+local enableKey = vim.keymap.set
+local disableKey = vim.keymap.del
 
 -- Move Lines
 -- Alternative Commands for MacOS "option/alt" Key not working
-map("n", "<C-m>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-map("n", "<C-n>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("i", "<C-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<C-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<C-n>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<C-m>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+enableKey("n", "<C-m>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+enableKey("n", "<C-n>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+enableKey("i", "<C-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+enableKey("i", "<C-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+enableKey("v", "<C-n>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+enableKey("v", "<C-m>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- Go (vim-go)
-map("n", "<leader>tg", ":GoTestFunc<cr>", { desc = "Testing Golang Function 🐰", silent = true, noremap = true })
+enableKey(
+  "n",
+  "<leader>tg",
+  ":GoTestFunc<cr>",
+  { desc = "Testing Golang Function 🐰", silent = true, noremap = true }
+)
 
 -- Enable Copilot
-map("n", "<leader>ce", ":Copilot enable<cr>", { desc = "Enable Copilot" })
-map("n", "<leader>cx", ":Copilot disable<cr>", { desc = "Disable Copilot" })
+enableKey("n", "<leader>ce", ":Copilot enable<cr>", { desc = "Enable Copilot" })
+enableKey("n", "<leader>cx", ":Copilot disable<cr>", { desc = "Disable Copilot" })
 
 -- Tmux Fix move CTRL+A to CTRL+Q
-map("n", "<C-q>", "<C-a>", { desc = "Increment Numbers", silent = true, noremap = true })
+enableKey("n", "<C-q>", "<C-a>", { desc = "Increment Numbers", silent = true, noremap = true })
 
 -- Tmux Navigator
-map("n", "<C-h>", ":<C-U>TmuxNavigateLeft<cr>", { desc = "Tmux Navigate Left", silent = true, noremap = true })
-map("n", "<C-j>", ":<C-U>TmuxNavigateDown<cr>", { desc = "Tmux Navigate Down", silent = true, noremap = true })
-map("n", "<C-k>", ":<C-U>TmuxNavigateUp<cr>", { desc = "Tmux Navigate Up", silent = true, noremap = true })
-map("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>", { desc = "Tmux Navigate Right", silent = true, noremap = true })
+enableKey("n", "<C-h>", ":<C-U>TmuxNavigateLeft<cr>", { desc = "Tmux Navigate Left", silent = true, noremap = true })
+enableKey("n", "<C-j>", ":<C-U>TmuxNavigateDown<cr>", { desc = "Tmux Navigate Down", silent = true, noremap = true })
+enableKey("n", "<C-k>", ":<C-U>TmuxNavigateUp<cr>", { desc = "Tmux Navigate Up", silent = true, noremap = true })
+enableKey("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>", { desc = "Tmux Navigate Right", silent = true, noremap = true })
+
+-- Disable LazyGit Keymaps
+disableKey("n", "<leader>gg")
+disableKey("n", "<leader>gG")
+disableKey("n", "<leader>gf")
+disableKey("n", "<leader>gl")
+disableKey("n", "<leader>gL")
