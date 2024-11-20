4864d7a1d67d547729e7a8469e70420f078fda3e
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-11-20 22:37:04 +0100

    update dashboard for new snacks dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,16 @@
+local logo = [[
+`8.`888b           ,8'  8 8888          ,8.       ,8.          
+ `8.`888b         ,8'   8 8888         ,888.     ,888.         
+  `8.`888b       ,8'    8 8888        .`8888.   .`8888.        
+   `8.`888b     ,8'     8 8888       ,8.`8888. ,8.`8888.       
+    `8.`888b   ,8'      8 8888      ,8'8.`8888,8^8.`8888.      
+     `8.`888b ,8'       8 8888     ,8' `8.`8888' `8.`8888.     
+      `8.`888b8'        8 8888    ,8'   `8.`88'   `8.`8888.    
+       `8.`888'         8 8888   ,8'     `8.`'     `8.`8888.   
+        `8.`8'          8 8888  ,8'       `8        `8.`8888.  
+         `8.`           8 8888 ,8'         `         `8.`8888. 
+]]
+
 -- define the colorscheme for nvim
 return {
   {
@@ -21,6 +34,23 @@ return {
     opts = {
       sections = {
         lualine_z = {},
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = logo,
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { icon = " ", title = "Recent Files", cwd = true, section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+          { section = "startup" },
+        },
       },
     },
   },
