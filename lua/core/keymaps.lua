b29d44636acc8810225b28f55084569863be9525
fschuermeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-12-18 15:20:00 +0100

    Work in Progress

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+
+local setKey = vim.keymap.set
+
+setKey("n", "<leader>q", ":q<CR>", { desc = "Exit Nvim" })
+
+-- Move Lines
+setKey("n", "<C-m>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+setKey("n", "<C-n>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+setKey("i", "<C-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+setKey("i", "<C-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+setKey("v", "<C-n>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+setKey("v", "<C-m>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+-- Diffview
+setKey("n", "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", { desc = "Diffview Current File" })
+setKey("n", "<leader>gh", "<cmd>DiffviewFileHistory<cr>", { desc = "Diffview History" })
+setKey("n", "<leader>gt", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
+setKey("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
+setKey("n", "<leader>gp", "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>", { desc = "Diffview Branch" })
---
+++ lua/core/options.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+-- Nvim Options
+-- Tab settings
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smarttab = true
+opt.autoindent = true
+opt.expandtab = true -- Use spaces instead of tabs
+
+-- Line Configs
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.wrap = false
+
