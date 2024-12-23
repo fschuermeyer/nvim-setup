42ec7f5e87f95bad925b616dbac3dee08e729cd7
fschuermeyer <25571323+fschuermeyer@users.noreply.github.com>
2024-12-23 03:19:13 +0100

    WIP: Switch Which Key Mode

--- lua/plugins/folke-which-key.lua
+++ lua/plugins/folke-which-key.lua
@@ -2,7 +2,7 @@ return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	opts = {
-        preset = "helix",
+        preset = "modern",
         sort = {"alphanum"},
 		spec = {
 			mode = { "n", "v" },
