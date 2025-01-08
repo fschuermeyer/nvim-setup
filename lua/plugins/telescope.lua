5ca337e3ca71555c3103935685ebf860c4bcc73f
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2025-01-08 16:37:25 +0100

    fix: remove colorizer plugin compatibility issues with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
         "nvim-tree/nvim-web-devicons",
         "folke/todo-comments.nvim",
         "BurntSushi/ripgrep",
