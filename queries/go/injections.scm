603ee05cc63db446ab8905c1a595c7fc2d817c9e
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2025-01-24 20:37:57 +0100

    feat(go): enhance SQL injection detection
    Extended the SQL injection detection pattern to include additional
    keywords such as TABLESAMPLE, ORDER BY, LIMIT, OFFSET, ON CONFLICT,
    FROM, INTO, VALUES, and SET. This improves the accuracy of detecting
    SQL injections in Go code.

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -4,6 +4,6 @@
     (raw_string_literal_content) 
     (interpreted_string_literal_content)
 ] @injection.content
-    (#match? @injection.content "(WITH|with|SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by|RETURNING|returning)")
+    (#match? @injection.content "(WITH|with|SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by|RETURNING|returning|TABLESAMPLE|tablesample|ORDER BY|order by|LIMIT|limit|OFFSET|offset|ON CONFLICT|on conflict|FROM|from|INTO|into|VALUES|values|SET|set)")
     (#set! injection.language "sql")
 )
