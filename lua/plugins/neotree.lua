18d3e893e5d2d0d3b833de17546d945f3e836683
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2025-02-10 00:36:47 +0100

    feat: update lualine and neotree configurations
    - Updated lualine to show full file path in filename section
    - Changed NeoTree toggle command to float instead of right

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,39 +1,39 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		local neo_tree = require("neo-tree")
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        local neo_tree = require("neo-tree")
 
-		neo_tree.setup({
-			window = {
-				mappings = {
-					-- fix: german layout
-					["<"] = "next_source",
-					[">"] = "prev_source",
-				},
-			},
-			filesystem = {
-				filtered_items = {
-					visible = true,
-				},
-			},
-			source_selector = {
-				winbar = true,
-			},
-		})
+        neo_tree.setup({
+            window = {
+                mappings = {
+                    -- fix: german layout
+                    ["<"] = "next_source",
+                    [">"] = "prev_source",
+                },
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+            source_selector = {
+                winbar = true,
+            },
+        })
 
-		local setKey = vim.keymap.set
+        local setKey = vim.keymap.set
 
-		setKey(
-			"n",
-			"<leader>e",
-			"<cmd>Neotree right toggle<cr>",
-			{ noremap = true, silent = true, desc = "Explorer NeoTree" }
-		)
-	end,
+        setKey(
+            "n",
+            "<leader>e",
+            "<cmd>Neotree float toggle<cr>",
+            { noremap = true, silent = true, desc = "Explorer NeoTree" }
+        )
+    end,
 }
