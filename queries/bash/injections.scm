b6b645edd4d89ccdcefef36301c8db1b3d27f3ab
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2025-02-15 19:20:10 +0100

    feat: add awk injection to bash queries
    Added support for awk injection in bash queries. This includes
    recognizing the command name "awk" and setting the injection
    language to "awk".

--- queries/bash/injections.scm
+++ queries/bash/injections.scm
@@ -4,3 +4,10 @@
   argument: (raw_string) @injection.content
   (#set! injection.language "jq")
 ) 
+
+;; Injection: awk
+(command
+  name: (command_name) @name (#match? @name "awk")
+  argument: (raw_string) @injection.content
+  (#set! injection.language "awk")
+) 
