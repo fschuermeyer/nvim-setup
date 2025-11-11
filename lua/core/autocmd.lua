6057904e1c59862c559d67d3e2be7c8ca613c758
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2025-11-11 18:31:34 +0100

    chore: update lazy-lock and refactor autocmds
    Updated `lazy-lock.json` to use the "canary" branch for `CopilotChat.nvim`
    and updated the commit hash for `LuaSnip`. Refactored `autocmd.lua` to
    improve readability and maintainability by replacing `vim.cmd` with
    `vim.api.nvim_create_autocmd` and consolidating file type definitions.

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,43 +1,39 @@
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#C11B3B", fg = "#F3A3B3" })
 
--- Highlight, was gejankt wurde
+-- Highlight for Yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "YankHighlight",
-            timeout = 200,
-        })
-    end,
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "YankHighlight",
+			timeout = 200,
+		})
+	end,
 })
 
--- Highlight for Template Files
-vim.cmd([[
-  autocmd BufNewFile,BufRead *.tmpl set filetype=gohtmltmpl
-  autocmd BufNewFile,BufRead *.gohtml set filetype=gohtmltmpl
-  autocmd BufNewFile,BufRead *.gohtmltmpl set filetype=gohtmltmpl
-  autocmd BufNewFile,BufRead *.gotexttmpl  set filetype=gotexttmpl
-  autocmd BufNewFile,BufRead *.twig set filetype=html
-  autocmd BufNewFile,BufRead *.zsh set filetype=bash
-]])
+-- File Type Definitions
+local filetype_patterns = {
+	["gohtmltmpl"] = { "*.tmpl", "*.gohtml", "*.gohtmltmpl" },
+	["gotexttmpl"] = { "*.gotexttmpl" },
+	["html"] = { "*.twig" },
+	["bash"] = { "*.zsh" },
+	["hcl"] = { "*.hcl", ".terraformrc", "terraform.rc" },
+	["terraform"] = { "*.tf", "*.tfvars" },
+	["json"] = { "*.tfstate", "*.tfstate.backup" },
+}
 
--- Highlight for Terraform Files
-vim.cmd([[
-  autocmd BufRead,BufNewFile *.hcl set filetype=hcl
-  autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl
-  autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform
-  autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json
-]])
+for filetype, patterns in pairs(filetype_patterns) do
+	vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+		pattern = patterns,
+		callback = function()
+			vim.bo.filetype = filetype
+		end,
+	})
+end
 
+-- Format on Save Additions
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.java",
-    callback = function()
-        vim.lsp.buf.format({ async = false })
-    end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.xml",
-    callback = function()
-        vim.lsp.buf.format({ async = false })
-    end,
+	pattern = { "*.java", "*.xml" },
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
 })
