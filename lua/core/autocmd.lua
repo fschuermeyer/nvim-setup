5ade240abb345874e38c9f9712ef3e1778a25b47
Felix Schürmeyer <25571323+fschuermeyer@users.noreply.github.com>
2026-04-17 11:40:15 +0200

    feat: set env variables from nvim

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,3 +1,22 @@
+-- Export buffer context as env vars for external tools (Task, Crush, etc.)
+local function update_nvim_env()
+	local file = vim.fn.expand("%:p")
+	local ft = vim.bo.filetype
+	local rel = vim.fn.expand("%:.")
+	local dir = vim.fn.expand("%:p:h")
+	local ext = vim.fn.expand("%:e")
+
+	vim.env.NVIM_FILE = file ~= "" and file or nil
+	vim.env.NVIM_REL_FILE = rel ~= "" and rel or nil
+	vim.env.NVIM_FILE_DIR = dir ~= "" and dir or nil
+	vim.env.NVIM_FILETYPE = ft ~= "" and ft or nil
+	vim.env.NVIM_EXT = ext ~= "" and ext or nil
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+	callback = update_nvim_env,
+})
+
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#C11B3B", fg = "#F3A3B3" })
 
 -- Highlight for Yank
